e0ba3689bf3ffbec24465c1827cd48ff7e61bed1
yeliang <wx-yel@dtdream.com>
2018-10-08 16:52:48 +0800

    ++

--- app/src/main/java/com/yetland/bbclient/MainActivity.java
+++ app/src/main/java/com/yetland/bbclient/MainActivity.java
@@ -1,7 +1,5 @@
 package com.yetland.bbclient;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,14 +25,7 @@ public class MainActivity extends BaseActivity {
     Button mButton;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-        mEditText = findViewById(R.id.editText);
-        mEditText2 = findViewById(R.id.editText2);
-        mButton = findViewById(R.id.button);
+    protected void init() {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +33,18 @@ public class MainActivity extends BaseActivity {
                         mEditText2.getText().toString());
             }
         });
+    }
+
+    @Override
+    protected void findViews() {
+        mEditText = findViewById(R.id.editText);
+        mEditText2 = findViewById(R.id.editText2);
+        mButton = findViewById(R.id.button);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
     }
 
     @Override
